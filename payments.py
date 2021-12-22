959176482fa6e8c159978ef9b2df58d06eb90d18
Isabel Díaz Miranda <isabeldiazmiranda@MacBook-Air-de-Isabel-2.local>
2021-12-21 18:52:29 -0600

    211221 update

--- payments.py
+++ payments.py
@@ -1,20 +1,22 @@
-""" 
-import csv
-
-file = open('/Users/isabeldiazmiranda/OneDrive/Jeeves/PaymentsScript/STP.csv')
-csvreader = csv.reader(file)
-
-header = next(csvreader)
-print(header)
-
-rows = []
-for row in csvreader:
-    rows.append(row)
-print(rows)
-file.close()
-"""
-
 import pandas as pd
 
-data= pd.read_csv('/Users/isabeldiazmiranda/OneDrive/Jeeves/PaymentsScript/STP.csv')
-print(data)
+
+data1 = pd.read_csv('/Users/isabeldiazmiranda/OneDrive/Jeeves/PaymentsScript/STP.csv', encoding= 'unicode_escape')
+STP = data1.filter(['org_id', 'date', 'amount_platform', 'amount_local'], axis=1)
+STP['bank_code'] = "STP"
+STP.insert(1,'stmt_ID',"")
+STP.insert(2,'stmt_status',"")
+STP.insert(4,'pmt_currency',"MXN")
+
+data2 = pd.read_csv('/Users/isabeldiazmiranda/OneDrive/Jeeves/PaymentsScript/RBC.csv', encoding= 'unicode_escape')
+RBC = data2.filter(['org_id', 'date', 'amount_platform', 'amount_local'], axis=1)
+RBC['bank_code'] = "RBC"
+RBC.insert(1,'stmt_ID',"")
+RBC.insert(2,'stmt_status',"")
+RBC.insert(4,'pmt_currency',"")
+
+frames = [STP, RBC]
+allPayments = pd.concat(frames)
+allPayments.to_csv('paymentsToCMS.csv')
+
+#print(allPayments)
